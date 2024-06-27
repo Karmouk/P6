8eb568dd30d67498c03075e59008b633df098197
LucasR <149654268+Karmouk@users.noreply.github.com>
2024-06-27 09:47:08 +0200

    travaux récupérés

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
@@ -16,6 +16,18 @@ getWorks()
 
 function displayWorks(work){
   const getGallery = document.querySelector(".gallery");
+  work.forEach(work => {
+    const figure = document.createElement('figure');
+    const img = document.createElement('img');
+    img.src=work.imageUrl
+    const figcaption = document.createElement('figcaption');
+    figcaption.textContent = work.title;
+    
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    getGallery.appendChild(figure);
+  })
+
 }
 
 function getCategories(){
